9584a761ae33c37ebf2243a7d76103b1788fe53e
fahadalisyed <fahadalisyed7@gmail.com>
2023-09-01 08:31:37 -0700

    first commit

---
+++ java 2/index.js
@@ -0,0 +1,31 @@
+var message='Hello..world';
+alert(message);
+
+
+
+var username='Jhon Deo';
+var age='15 year old';
+var profession='certified mobile application development';
+alert(username);
+alert(age);
+alert(profession);
+
+
+
+var pizza='pizza\npizz\npiz\npi\np';
+alert(pizza);
+
+
+var email='my email is fahadalisyed7@gmail. com';
+alert(email);
+
+
+var book='I am trying to learn from the Book a smarter\n way to learn java script';
+alert(book);
+
+
+document.write( 'Yah! I can write HTML content through Java Script');
+
+
+var design='“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”';
+alert(design)
